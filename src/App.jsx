373541a738d1f1db70a70f48a08b5f5bc91b501c
aaaziim <azim210215@gmail.com>
2024-12-04 00:20:14 +0600

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,15 @@
+import './App.css'
+
+function App() {
+  const [count, setCount] = useState(0)
+
+  return (
+    <>
+      <h1 className="text-3xl font-bold underline bg-red-200 p-4 text-center">
+        Hello world!
+      </h1>
+    </>
+  )
+}
+
+export default App
